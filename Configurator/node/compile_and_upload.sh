467a0a9357d4583287b5d7c6445d866508c85d69
Martin Sárkány <martinsarkany@gmail.com>
2018-05-05 18:57:50 +0200

    Update configuration of all nodes

--- Configurator/node/compile_and_upload.sh
+++ Configurator/node/compile_and_upload.sh
@@ -3,17 +3,18 @@
 PATH_FILE=$EDU_HOC_HOME/config/relay_ar_paths.txt
 JAR=/home/martin/Desktop/Edu-hoc/JeeTool/JeeTool/dist/JeeTool.jar
 CLASS=cz.muni.fi.crocs.EduHoc.Main
-PROJECT=/home/martin/git/wsnpl_arduino/Configurator/node
+#PROJECT=/home/martin/git/wsnpl_arduino/Configurator/node
+PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 echo "$J0" > $PATH_FILE
 echo "$J1" >> $PATH_FILE
 echo "$J2" >> $PATH_FILE
 echo "$J3" >> $PATH_FILE
-#echo "$J4" >> $PATH_FILE
+echo "$J4" >> $PATH_FILE
 echo "$J5" >> $PATH_FILE
 echo "$J6" >> $PATH_FILE
-#echo "$J7" >> $PATH_FILE
+echo "$J7" >> $PATH_FILE
 
 java -cp $JAR $CLASS -a $PATH_FILE -u $PROJECT
 
